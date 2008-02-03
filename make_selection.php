7b66125038359c8b8e8cf5626de63831299284f3
kastner <kastner@b2180d9d-0f15-4909-a4fa-37428cf2907f>
2008-02-03 16:39:23 +0000

    inital import
    git-svn-id: http://opus.winelibrary.com/highlite@1 b2180d9d-0f15-4909-a4fa-37428cf2907f

---
+++ make_selection.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" xml:lang="en">
+<head>
+  <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+  <title>Text + Image + CSS3 = Crazy Delicious</title>
+  <script src="jquery.js" type="text/javascript" charset="utf-8"></script>
+  <style type="text/css" media="screen">
+    body {
+      font: small "Helvetica", "Trebuchet MS", Verdana, sans-serif;
+    }
+    #form textarea {
+      width: 300px;
+      height: 30px;
+      margin-left: 20px;
+      margin-bottom: 10px;
+    }
+    #form select {
+      margin-left: 20px;
+    }
+    #form label {
+      display: block;
+    }
+    #form input {
+      width: 300px;
+      margin-left: 20px;
+      margin-bottom: 10px;
+    }
+    span::selection {
+      color: white;
+      background: white
+    }
+    span::-moz-selection {
+      color: white;
+      background: white
+    }
+    #beg {
+      float: right;
+      width: 15em;
+      margin-right: 4em;
+      padding: 0.5em;
+      border: 4px double #999;
+      background: #efefef;
+    }
+    #code {
+      width: 300px;
+      height: 40px;
+    }
+  </style>
+</head>
+<body id="color">
+  <h1>Hide an image in html</h1>
+  <script type="text/javascript"><!--
+  google_ad_client = "pub-5369189596696267";
+  /* highlite - 2/2/8 */
+  google_ad_slot = "0346720180";
+  google_ad_width = 728;
+  google_ad_height = 15;
+  //-->
+  </script>
+  <script type="text/javascript"
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+  </script>
+  <h2>Highlight the text with your mouse.</h2>
+  
+  <div id="beg">
+    <p>
+      Like this or my other projects like <a href="http://metaatem.net/words/spell+with+flickr" alt="Spell with Flickr">Spell with Flickr</a> and <a href="http://befuddlr.com/" alt="Befuddlr!">Befuddlr</a>? Help me cover the cost of EC2:
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+      <input type="hidden" name="cmd" value="_s-xclick">
+      <input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+      <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+      <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHPwYJKoZIhvcNAQcEoIIHMDCCBywCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBQ6a6PLjlQp+glExehijfovjujVsuhPW8Rpoe400/R1RpRLQ1wB8ryhde7w5AqZCFGvSLmqXCtmGGpCV0dZhO45GESygZHe64OOUDfTCfSpIl3j6AmRQjP0vmVv3VOsW0paWObugPDQnTsm68hMwIWqUqPHOQTghofvsln/BZnkTELMAkGBSsOAwIaBQAwgbwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIkIfevVnNXw6AgZhfCroHHM/J9ODC4VgjXcUSD8nsXOIHaJWpG76iurzOVq1b+OngVHuV59EuZHcAeYMZiyTo1aHud1bLi6y4yl2MW9pT9j/jDBGbQy79vE/ODbayMvp3lM3pQUNNaPhXn77yFU/6tFp7LeHucjDro965ERokv3k0XGzV5XXuzLBspgg1DgLzy0nksCHqomhEa7umkqmhtOPWy6CCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTA4MDIwMjIzNTc1NFowIwYJKoZIhvcNAQkEMRYEFIkCzbAhsr4v9YtTx1tUQmhDyEnhMA0GCSqGSIb3DQEBAQUABIGAp60E1zYm9NxknDPxr6GZd/CjMYHtcoQVA4kJmhWO3+AM/Ot9E4ry9ixJ8OC6P0FciQhH17vgFRqXHEGlYzW3y3Ok6qoqv3rm6EZ3shbz5QpDTKyru4p0G2i5SC3IDQiVXj9XXWJOqGF73IAeLXhMrjwhOPPZNBUMqZAAJTPDZKk=-----END PKCS7-----
+      ">
+      </form>
+    </p>
+    <p>Created by <a href="mailto:kastner@gmail.com">Erik Kastner</a> at 2am</p>
+  </div>
+  
+  <h3>How it works:</h3>
+  <p>Safari and Firefox support <a href="http://www.w3.org/TR/css3-selectors/#UIfragments" title="Selectors">CSS3's ::selection pseudo-element</a>. Using that and PHP, this toy hides an image in plain view.</p>
+  <form id="form">
+    <div>    
+      <label for="text">Text:</label>
+      <textarea id="text" name="text"><?php echo $_REQUEST["text"] ?></textarea>
+    </div>  
+    <div>    
+      <label for="image">Image URL:</label>
+      <input type="text" id="image" name="image" value="<?php echo $_REQUEST["image"] ?>">
+    </div>
+    <div>
+      <label for="colors">Number of colors:</label>
+      <input type="hidden" id="h-colors" value="<?php echo $_REQUEST["colors"]; ?>"/>
+      <select id="colors" name="colors">
+        <option>2</option>
+        <option>4</option>
+        <option>16</option>
+        <option>32</option>
+        <option selected>128</option>
+        <option>256</option>
+      </select>
+    </div>
+    <div>
+      <label for="width">Width of output:</label>
+      <input type="hidden" id="h-width" value="<?php echo $_REQUEST["width"]; ?>"/>
+      <select id="width" name="width">
+        <option>40</option>
+        <option>80</option>
+        <option selected>120</option>
+        <option>160</option>
+        <option>220</option>
+      </select>
+    </div>
+    <input type="submit" value="generate"/>
+  </form>
+  <div id="output-code" style="display:none;">
+    <div>
+      <a href="#" onclick="$('#code').toggle(); return false;">Get code</a>
+    </div>
+    <textarea id="code" style="display:none;" onfocus="this.select();"></textarea>
+  </div>
+  <div id="output"><?php if ($_REQUEST["image"]) echo "Loading..." ?></div>
+  <script type="text/javascript" charset="utf-8">  
+    function go() {
+      if (!$("#image").val().match(/^http:\/\//)) {
+        alert("You must supply a url for an image");
+        return false;
+      }
+      $("#output").load("make_selection_ajax.php", {
+        text: $("#text").val(),
+        image: $("#image").val(),
+        colors: $("#colors").val(),
+        width: $("#width").val()
+      }, function(responseText) {
+        $("#output-code").show();
+        $("#code").val(responseText);
+        new_css = responseText.match(/\/\*--\*\/(\n|.)+\/\*/);
+        if (new_css) {
+          $("#injectedCss").html(new_css[0]);
+        }
+      });      
+    }
+    
+    $(document).ready(function() {
+  		$('head').append('<style id="injectedCss" type="text/css">.bob{border:0}</style>');
+  		if ($("#h-colors").val() != "") { $("#colors").val($("#h-colors").val()); }
+  		if ($("#h-width").val() != "") { $("#width").val($("#h-width").val()); }
+  		if ($("#image").val() != "") {
+  		  go();
+  		}
+  	});
+  </script>  
+  <script type="text/javascript">
+  var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+  document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+  </script>
+  <script type="text/javascript">
+  var pageTracker = _gat._getTracker("UA-70669-14");
+  pageTracker._initData();
+  pageTracker._trackPageview();
+  </script>
+</body>
+</html>
---
+++ make_selection_ajax.php
@@ -0,0 +1,142 @@
+<?php
+// grab text from the user
+$text = stripslashes($_REQUEST["text"]);
+
+// default text if non supplied
+if (!$text) { $text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."; }
+
+// grab the suggested # of colors
+$num_colors = $_REQUEST["colors"];
+$num_colors = intval($num_colors);
+if ($num_colors > 1024 or $num_colors < 2) { $num_colors = 32; }
+
+// output width
+$width = $_REQUEST["width"];
+$width = intval($width);
+if ($width > 2000 or $width < 40) { $width = 120; }
+
+// grab image from user
+$image = $_REQUEST["image"];
+
+// check for valid url
+if (!preg_match("/http:\/\//", $image)) { die("You must have a valid url for the image"); }
+
+// fetch the image size
+$image_size = getimagesize($image);
+$image_height = $image_size[1];
+$image_width = $image_size[0];
+
+// get the image into memory, based on the mime-type
+if (!preg_match("/\/(jpe?g|png|gif)$/", $image_size["mime"], $type)) { die("You must submit a jpeg, gif or png"); }
+
+switch($type[1]) {
+  case "jpeg":
+  case "jpg":
+    $image = imagecreatefromjpeg($image);
+    break;
+  case "gif":
+    $image = imagecreatefromgif($image);
+    break;
+  case "png":
+    $image = imagecreatefrompng($image);
+}
+
+// dither the image down to suggested # of colors
+imagetruecolortopalette($image, true, $num_colors);
+
+// calculate the width (1:2 the original)
+$new_width = $width;
+$new_height = ceil(($new_width / $image_width) * $image_height / 2);
+
+// make a new memory image
+$image2 = imagecreate($new_width, $new_height);
+
+// squish (or stretch) the image into the new image object
+imagecopyresized($image2, $image, 0, 0, 0, 0, $new_width, $new_height, $image_width, $image_height);
+
+// pad the text if needed
+$text = preg_replace("/\n|\t+/", "", $text);
+$new_text = $text;
+$total_chars = $new_width * $new_height;
+if ($total_chars > strlen($text)) {
+  for ($i=0; $i<=round($total_chars / strlen($text)); $i++) {
+    $new_text .= $text;
+  }  
+}
+
+// get the text into an array
+$lines = preg_split("/(.{" . $width . "})/", $new_text, -1, PREG_SPLIT_DELIM_CAPTURE);
+foreach($lines as $line) {
+  if ($line != "") { $real_lines[] = $line; }
+}
+$lines = $real_lines;
+// var_dump($lines);
+$colors = array();
+$output = "";
+
+// iterate through the lines in the image
+for ($y = 0; $y < $new_height; $y++) {
+
+  $line = rtrim($lines[$y]);
+  
+  // get the characters in the line into an array
+  $chars = preg_split("/(.)/", $line, -1, PREG_SPLIT_DELIM_CAPTURE);
+  $real_chars = array();
+  foreach($chars as $char) { if ($char != "") { $real_chars[] = $char; } }
+  $chars = $real_chars;
+
+  // (re)set the last color to group colors together
+  $last_color = -1;
+  $output .= "<div id='line-$y'>";
+
+  // iterate through the columns in the image
+  for ($x = 0; $x < $new_width; $x++) {
+    
+    // look at current pixel
+    $pixel_color = imagecolorat($image2, $x, $y);
+    if (!$colors[$pixel_color]) {
+      $colors[$pixel_color] = imagecolorsforindex($image2, $pixel_color);
+    }
+    
+    // new color? close the last span, start a new one and set the last color
+    if ($last_color != $pixel_color) {
+      if ($last_color != -1) { $output .= "</span>"; }
+      $last_color = $pixel_color;
+      $output .= "<span class='color-$pixel_color'>";
+    }
+    
+    // add the current character
+    $output .= $chars[$x];
+  }
+  
+  if ($last_color != -1) { $output .= "</span>"; }
+  $output .= "</div>";
+}
+
+// get the colors into css
+foreach($colors as $color => $rgb) {
+  if ($rgb["alpha"] == 0) {
+    // $style .= ".color-$color { color: rgb($rgb[red], $rgb[green], $rgb[blue])}\n";
+    $style .= <<<CSS
+    
+      .color-$color::selection {
+        color: rgb($rgb[red], $rgb[green], $rgb[blue]);
+        background: rgb($rgb[red], $rgb[green], $rgb[blue])
+      }
+      .color-$color::-moz-selection {
+        color: rgb($rgb[red], $rgb[green], $rgb[blue]);
+        background: rgb($rgb[red], $rgb[green], $rgb[blue])
+      }
+      
+CSS;
+  }
+}
+?>
+<pre id="sel">
+<?php echo $output ?>
+</pre>
+<style type="text/css" media="screen">
+/*--*/
+  <?php echo $style ?>
+/*!!*/
+</style>
